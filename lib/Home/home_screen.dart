85ac06897a5f3d7afe596264096dafbb7287fff9
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-08-03 22:06:45 +0900

    add: Screenを作成

--- lib/Home/home_screen.dart
+++ lib/Home/home_screen.dart
@@ -1,10 +1,75 @@
+// import 'package:exchange_contact_app/component/barcode_item/facebook_item.dart';
+// import 'package:exchange_contact_app/component/barcode_item/instagram_item.dart';
+// import 'package:exchange_contact_app/component/barcode_item/line_item.dart';
+// import 'package:exchange_contact_app/component/barcode_item/twiter_item.dart';
+// import 'package:exchange_contact_app/component/local/shared_preferences.dart';
+// import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+// import 'package:provider/provider.dart';
+// import 'home_screen_state.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: () {},
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          elevation: 3,
+          backgroundColor: Colors.white,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.account_circle,
+                color: Colors.blueAccent,
+                size: 40,
+              ),
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 15),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.contact_support_rounded,
+                  color: Colors.blueAccent,
+                  size: 40,
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Container(height: 50),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                children: [
+                  Text('1'),
+                  Text('2'),
+                  Text('3'),
+                  Text('4'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
